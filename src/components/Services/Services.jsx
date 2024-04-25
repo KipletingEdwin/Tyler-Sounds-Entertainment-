fcacfaf43987e721c1ec766cbbb82b9e19b03d7e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-25 14:43:25 +0300

    add events

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import styles from './Services.module.css';
+import services from '../../Data/services.json';
 
 function Services() {
   return (
     <section className={styles.container}>
        <h2 className={styles.title} > Services</h2>
+       <h4>Some of the services we offer include but not limited to:</h4>
        <div className={styles.services}>
-
-        <div>
-        <h6> Wedding DJ Services </h6>
-        <p> Create unforgettable wedding memories with our personalized DJ services, tailored to your unique love story and celebration. </p>
-        </div>
-
-        <div>
-        <h6> Corporate Events </h6>
-        <p> Set the perfect ambiance for your corporate event with our professional DJ services, delivering dynamic music and entertainment to elevate your brand presence. </p>
-        </div>
-
-
+        {
+          services.map((serv, id)=>(
+            <div key={id} className={styles.service}>
+              <h5>{serv.service}</h5>
+              <p>{serv.description}</p>
+            </div>
+          ))
+        }
        </div>
     </section>
   )
