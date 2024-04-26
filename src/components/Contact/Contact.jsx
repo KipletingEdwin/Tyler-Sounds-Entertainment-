e0dc82a8e15b07c07610440817805ced2555a0e8
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-26 16:00:55 +0300

    style the inputs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
-import styles from './Contact.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons';
-
-
+import React from "react";
+import styles from "./Contact.module.css";
+import contact from "../../assets/contact/contact.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
     <section className={styles.container}>
-        <div className={styles.contact}>
-            <h3> Talk to us </h3>
-            <div className={styles.icons}>
-            <FontAwesomeIcon icon={faEnvelope} />
-            <FontAwesomeIcon icon={faPhone} />
-            <FontAwesomeIcon icon={faLocationDot} />
+      <div className={styles.contact}>
+        <div className={styles.iconsContainer}>
+          <img src={contact} alt="contact" />
+          <div className={styles.icons}>
+            <div className={styles.iconsDiv} >
+            <FontAwesomeIcon icon={faEnvelope}  className={styles.faEnvelope} />
+            <a href="tylersoundsentertainment@gmail.com"> <p>tylersoundsentertainment@gmail.com</p> </a>
             </div>
-            <div className={styles.contactForm}>
-                <input type='text' placeholder='Name' required />
-                <input type='email' placeholder='Email' required />
-                <input type='text' placeholder='Subject' required />
-                <textarea placeholder='Message' required></textarea>
-                <button>Send Message</button>
-
+            <div className={styles.iconsDiv}>
+            <FontAwesomeIcon icon={faPhone} className={styles.faPhone} /> <p>+254728012535</p>
             </div>
-        
+           
+            
+          </div>
         </div>
-        
+
+        <div className={styles.contactForm}>
+            <h3>Get in touch with us</h3>
+          <input type="text" placeholder="Name" required />
+          <input type="email" placeholder="Email" required />
+          <input type="text" placeholder="Subject" required />
+          <textarea placeholder="Message" required></textarea>
+          <button>Send Message</button>
+        </div>
+
+      </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
