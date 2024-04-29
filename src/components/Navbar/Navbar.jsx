fbd3fc55a3b7051bfa20707e0c9af7183296aa6c
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-29 15:22:06 +0300

    change anchor tags to Link on Video Mixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,14 @@ import styles from "./Navbar.module.css";
 import logo from "../../assets/nav/logo.jpg";
 import closeIcon from "../../assets/nav/closeIcon.png";
 import closeButton from "../../assets/nav/menuIcon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGreaterThan } from "@fortawesome/free-solid-svg-icons";
+
 import { Link } from "react-router-dom";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const[hoveredItem, setHoveredItem] = useState(null);
+  const [hoveredItem, setHoveredItem] = useState(null);
 
   const handleMouseEnter = (item) => {
     setHoveredItem(item);
@@ -42,48 +45,81 @@ function Navbar() {
           <li>
             <Link to="/events">Booking</Link>
           </li>
-          <li onMouseEnter={()=>handleMouseEnter('audioMixes')} onMouseLeave={handleMouseLeave} >
-            <Link to="/audioMixes">Audio Mixes</Link>
-            {
-              hoveredItem === 'audioMixes' && (
-                <div className={styles.children}>
+          <li
+            onMouseEnter={() => handleMouseEnter("audioMixes")}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div className={styles.subDiv}>
+              <Link to="/audioMixes" className={styles.link}>
+                Audio Mixes
+                <FontAwesomeIcon
+                  icon={faGreaterThan}
+                  className={styles.faGreaterThan}
+                />
+              </Link>
+            </div>
+
+            {hoveredItem === "audioMixes" && (
+              <div className={styles.children}>
                 <ul>
-                  <li> <Link to="/2024Mixes"> 2024 Mixes</Link></li>
-                  <li> <Link to="/2023Mixes"> 2023 Mixes</Link></li>
-                  <li> <Link to="/2022Mixes"> 2022 Mixes</Link></li>
-                  <li> <Link to="/2021Mixes"> 2021 Mixes</Link></li>
-                  <li> <Link to="/2020Mixes"> 2020 Mixes</Link></li>
-                  <li> <Link to="/2019Mixes"> 2019 Mixes</Link></li>
-                  <li> <Link to="/2018Mixes"> 2018 Mixes</Link></li>
-                  <li> <Link to="/2017Mixes"> 2017 Mixes</Link></li>
+                  <li>
+                    <Link to="/2024Mixes"> 2024 Mixes</Link>
+                  </li>
+                  <li>
+                    <Link to="/2023Mixes"> 2023 Mixes</Link>
+                  </li>
+                  <li>
+                    <Link to="/2022Mixes"> 2022 Mixes</Link>
+                  </li>
+                  <li>
+                    <Link to="/2021Mixes"> 2021 Mixes</Link>
+                  </li>
+                  <li>
+                    <Link to="/2020Mixes"> 2020 Mixes</Link>
+                  </li>
+                  <li>
+                    <Link to="/2019Mixes"> 2019 Mixes</Link>
+                  </li>
+                  <li>
+                    <Link to="/2018Mixes"> 2018 Mixes</Link>
+                  </li>
+                  <li>
+                    <Link to="/2017Mixes"> 2017 Mixes</Link>
+                  </li>
                 </ul>
               </div>
-              )
-            }
+            )}
           </li>
 
-          <li onMouseEnter={()=>handleMouseEnter('videoMixes')} onMouseLeave={handleMouseLeave} >
-            <Link to="/videoMixes">Video Mixes</Link>
-            {
-              hoveredItem === 'videoMixes' && (
-
-                <div className={styles.withChildren}>
+          <li
+            onMouseEnter={() => handleMouseEnter("videoMixes")}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div className={styles.subDiv}>
+              <Link to="/videoMixes">
+                Video Mixes
+                <FontAwesomeIcon
+                  icon={faGreaterThan}
+                  className={styles.faGreaterThan}
+                />
+              </Link>
+            </div>
+            {hoveredItem === "videoMixes" && (
+              <div className={styles.withChildren}>
                 <ul>
-                  <li>2024 Mixes</li>
-                  <li>2023 Video Mixes</li>
-                  <li>2022 Video Mixes</li>
-                  <li>2021 Video Mixes</li>
-                  <li>2020 Video Mixes</li>
-                  <li>2019 Video Mixes</li>
-                  <li>2018 Video Mixes</li>
-                  <li>2017 Video Mixes</li>
+                  <li> <Link to="/2024VideoMixes">2024 Mixes</Link> </li>
+                  <li> <Link to="/2023VideoMixes">2023 Video Mixes</Link> </li>
+                  <li> <Link to="/2022VideoMixes">2022 Video Mixes </Link></li>
+                  <li> <Link to="/2021VideoMixes">2021 Video Mixes</Link> </li>
+                  <li> <Link to="2020VideoMixes">2020 Video Mixes</Link> </li>
+                  <li> <Link to="/2019VideoMixes">2019 Video Mixes</Link> </li>
+                  <li> <Link to="201VideoMixes">2018 Video Mixes</Link> </li>
+                  <li> <Link to="2017VideoMixes">2017 Video Mixes</Link> </li>
                 </ul>
               </div>
-              )
-            }
+            )}
           </li>
 
-     
           <li>
             <Link to="/contact">Contact</Link>
           </li>
