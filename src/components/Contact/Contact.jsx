657ad2d8fcf0acac58a88146d07658153028dc9d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-29 09:14:40 +0300

    add functionality to contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Contact.module.css";
 import contact from "../../assets/contact/contact.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      formData.name === "" ||
+      formData.email === "" ||
+      formData.subject === "" ||
+      formData.message === "" 
+    ){
+      alert("Please fill in all fields")
+    } else{
+      fetch(" ",{
+        method: "POST",
+        headers:{
+          "Content-Type": "appllication/json"
+        },
+        body: JSON.stringify(formData),
+      })
+      .then((response)=>response.json())
+      .then((data)=>console.log(data));
+    }
+
+  }
 
   return (
     <section className={styles.container}>
@@ -13,27 +47,32 @@ function Contact() {
         <div className={styles.iconsContainer}>
           <img src={contact} alt="contact" />
           <div className={styles.icons}>
-            <div className={styles.iconsDiv} >
-            <FontAwesomeIcon icon={faEnvelope}  className={styles.faEnvelope} />
-            <a href="tylersoundsentertainment@gmail.com"> <p>tylersoundsentertainment@gmail.com</p> </a>
+            <div className={styles.iconsDiv}>
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className={styles.faEnvelope}
+              />
+              <a href="tylersoundsentertainment@gmail.com">
+                <p>tylersoundsentertainment@gmail.com</p>{" "}
+              </a>
             </div>
             <div className={styles.iconsDiv}>
-            <FontAwesomeIcon icon={faPhone} className={styles.faPhone} /> <p>+254728012535</p>
+              <FontAwesomeIcon icon={faPhone} className={styles.faPhone} />{" "}
+              <p>+254728012535</p>
             </div>
-           
-            
           </div>
         </div>
 
         <div className={styles.contactForm}>
-            <h3>Get in touch with us</h3>
-          <input type="text" placeholder="Name" required />
-          <input type="email" placeholder="Email" required />
-          <input type="text" placeholder="Subject" required />
-          <textarea placeholder="Message" required></textarea>
-          <button>Send Message</button>
+          <h3>Get in touch with us</h3>
+          <form  className={styles.form} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="name" required onChange={handleChange} />
+            <input type="email" placeholder="Email" name="email" onChange={handleChange} required />
+            <input type="text" placeholder="Subject" name="subject" onChange={handleChange} required />
+            <textarea placeholder="Message" name="message" required onChange={handleChange}></textarea>
+            <button>Send Message</button>
+          </form>
         </div>
-
       </div>
     </section>
   );
